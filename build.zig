bf7599f0c3be32eed0c4ec4dee5e8937f856c3a9
Jack Parsons <jackpar1113@gmail.com>
2026-03-16 20:05:55 -0600

    make it easier to run examples

--- build.zig
+++ build.zig
@@ -2,20 +2,35 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
-    _ = b.addModule("zoto", .{
+    const optimize = b.standardOptimizeOption(.{});
+    const mod = b.addModule("zoto", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
     });
 
     const mod_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-        }),
+        .root_module = mod,
     });
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
+
+    // Example executable
+    const example = b.addExecutable(.{
+        .name = "zoto-example",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("examples/person/src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "zoto", .module = mod },
+            },
+        }),
+    });
+
+    const example_step = b.step("example", "Run the example");
+    const run_example = b.addRunArtifact(example);
+    run_example.step.dependOn(b.getInstallStep());
+    example_step.dependOn(&run_example.step);
 }
